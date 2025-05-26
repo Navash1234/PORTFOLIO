3ab5ac8d63c527f63b0886c5895ea5ed6fab7091
Navash Ahamed <navasahamed319@gmail.com>
2025-05-26 17:22:59 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,123 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+
+// export const HERO_CONTENT = `I am a passionate Front-End Developer with a knack for designing and building visually appealing, responsive, and user-centric web interfaces. With a strong foundation in modern front-end technologies such as React, Next.js, and Tailwind CSS, I specialize in turning creative concepts into functional and intuitive user experiences. My expertise in crafting seamless, accessible, and interactive designs enables me to deliver solutions that not only meet business goals but also delight users.
+
+// Driven by a commitment to excellence and innovation, I aim to bridge the gap between design and functionality, creating web applications that stand out in performance and usability.`;
+
+// export const ABOUT_TEXT = `I am a dedicated Front-End Developer with a strong focus on building responsive and efficient web applications. Proficient in modern technologies like React, Next.js, and Tailwind CSS, I specialize in creating dynamic and scalable interfaces that enhance user engagement. I am passionate about writing clean, maintainable code and delivering high-performance solutions. Collaboration and problem-solving are at the core of my approach, allowing me to contribute effectively to team projects. I enjoy staying updated with emerging technologies and continuously improving my skills to build impactful web applications.Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+// export const EXPERIENCES = [
+//     {
+//       year: "September 2024 - December 2024",
+//       role: "Intermediate Web Developer Intern",
+//       company: "Agrownet",
+//       description: `Developed and maintained web applications using JavaScript, HTML, and CSS to enhance user experience. Collaborated with the team to implement new features and optimize platform performance. Contributed to front-end development, focusing on responsive design for device compatibility.`,
+//       technologies: ["HTML", "CSS", "JavaScript", "React.js", "Node.js"],
+//     },
+//     {
+//       year: "December 2024 - Present",
+//       role: "Software Development Intern",
+//       company: "NullClass",
+//       description: `Contributed to project development using React, Redux, and modern web technologies. Collaborated with the team to implement new features, optimize performance, and address bugs. Gained hands-on experience in full-stack development and enhanced problem-solving skills through practical tasks.`,
+//       technologies: ["React", "Redux", "JavaScript", "Node.js"],
+//     },
+// ];
+
+// export const PROJECTS = [
+//   {
+//     title: "YouTube Clone",
+//     image: project1,
+//     description: 
+//       "Developed a responsive YouTube clone with video grid layout, search functionality, and navigation. Used Grid and Flexbox to design interactive components like video cards and notifications. Recreated YouTube’s core UI features, including header, sidebar, and video previews with channel info.",
+//     technologies: ["HTML5", "CSS3"]
+//   },
+//   {
+//     title: "E-Commerce Website",
+//     image: project2,
+//     description:
+//       "Engineered a responsive Amazon clone with e-commerce features, including shopping cart functionality. Developed unit tests using the Jasmine framework to ensure code reliability and maintainability. Implemented a mobile-responsive design with modular architecture, dynamic product rendering, and an interactive cart system.",
+//     technologies: ["HTML5", "CSS3", "JavaScript" , "Jasmine"]
+//   },
+//   {
+//     title: "Stack Overflow Platform",
+//     image: project3,
+//     description:
+//       "Developed a Stack Overflow clone with user authentication, question posting, and commenting features. Utilized React and Redux for dynamic, state-driven UI components and efficient data management. Built RESTful APIs with Node.js, connecting to MongoDB for storing and managing user and question data.",
+//      technologies: ["React", "Redux", "JavaScript", "HTML", "CSS", "Node.js", "MongoDB"]
+//   },
+//   {
+//     title: "Google Clone",
+//     image: project4,
+//     description: 
+//       "Recreated Google’s homepage using Tailwind CSS, including search bar functionality, logo, and simple footer design and added darkmode functionality. Designed with mobile-first responsiveness in mind.",
+//     technologies: ["Tailwind CSS", "JavaScript"]
+//   }
+// ];
+
+// export const CONTACT = {
+//   address: "Coimbatore , TamilNadu",
+//   phoneNo: "+91 9865828638 ",
+//   email: "yeeswanth@gmail.com",
+// };
+export const HERO_CONTENT = `I am a dedicated Software Developer skilled in building scalable and user-friendly web applications using the MERN stack. With a strong foundation in JavaScript, React, and Node.js, I focus on developing responsive, efficient, and impactful solutions that align with real-world needs. I bring hands-on experience from internships and projects that bridge front-end creativity and back-end logic. My aim is to contribute to dynamic teams by delivering innovative software that enhances user experience and drives business success.`;
+
+export const ABOUT_TEXT = `I am a final-year Computer Science Engineering student with practical experience in software development, data analytics, and web technologies. Proficient in the MERN stack, REST APIs, and agile practices, I specialize in building intuitive and scalable web applications. I enjoy problem-solving, collaborating in team environments, and staying updated with the latest in tech. I'm passionate about continuous learning, open-source contribution, and developing solutions that positively impact users.`;
+
+export const EXPERIENCES = [
+  {
+    year: "August 2024 - September 2024",
+    role: "Software Developer Intern",
+    company: "MindzCube",
+    description: `Worked on developing a College Management System using the MERN stack. Implemented secure user authentication and CRUD operations for students, faculty, and admins. Built responsive and dynamic UI for internal and external mark viewing, materials upload, and timetable management.`,
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "JWT", "bcrypt.js", "Tailwind CSS"]
+  },
+  {
+    year: "Feb 2025 – Mar 2025",
+   role: "Data Analyst Intern",
+   company: "Infosys Springboard",
+   description: ` Data Collection and Preparation & Power BI Integration (Feb 2025): Gathered air quality data, fixed missing and incorrect data, categorized data for trends, and built an interactive Power BI dashboard.
+•  Streamlit Integration (Feb - Mar 2025): Developed a Streamlit web application to host and embed the Power BI dashboard, ensuring seamless user interaction.`,
+   technologies: ["Python", "Power BI", "Streamlit"]
+  },
+  {
+    year: "November 2024",
+    role: "Virtual Software Engineering Intern",
+    company: "Accenture North America",
+    description: `Completed virtual job simulation including designing web applications, improving code quality, debugging, and writing test cases. Strengthened practical development skills with a focus on teamwork and agile development.`,
+    technologies: ["HTML", "CSS", "JavaScript", "Git"]
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "Automated Hallticket process",
+    image: project1,
+    description:
+      "Automated the end-to-end examination process including exam registration, hall ticket generation, fee validation, and exam scheduling. Implemented conflict-free exam scheduling using genetic algorithms and real-time notifications with NodeMailer.",
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "JWT", "Tailwind CSS"]
+  },
+  {
+    title: "College Management System",
+    image: project2,
+    description:
+      "Developed a full-featured MERN Stack application with role-based access for students, faculty, and admins. Enabled internal/external mark tracking, timetable management, course material uploads, and account control through a secure admin panel.",
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "JWT", "Tailwind CSS"]
+  },
+  {
+    title: "Air Quality Monitoring & Dashboard Integration",
+    image: project3,
+    description: 
+    "Created a data-driven air quality monitoring solution during Infosys Springboard internship. Processed and cleaned environmental data, built trend-based visual analytics using Power BI, and developed a Streamlit web app for interactive dashboard deployment.",
+    technologies: ["Python", "Power BI", "Streamlit"]
+  }
+];
+
+export const CONTACT = {
+  address: "Tamil Nadu, India",
+  phoneNo: "+91 7397605800",
+  email: "navasahamed319@gmail.com",
+};
